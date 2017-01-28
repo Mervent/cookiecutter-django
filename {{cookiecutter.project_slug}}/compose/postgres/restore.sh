906f509cd70ec9d073504d9a7ac2c6c8c6fe76fe
mervent <lamkaslim@gmail.com>
2017-01-28 23:25:32 +0300

    Postgres now using django database instead of default

--- {{cookiecutter.project_slug}}/compose/postgres/restore.sh
+++ {{cookiecutter.project_slug}}/compose/postgres/restore.sh
@@ -3,14 +3,6 @@
 # stop on errors
 set -e
 
-# we might run into trouble when using the default `postgres` user, e.g. when dropping the postgres
-# database in restore.sh. Check that something else is used here
-if [ "$POSTGRES_USER" == "postgres" ]
-then
-    echo "restoring as the postgres user is not supported, make sure to set the POSTGRES_USER environment variable"
-    exit 1
-fi
-
 # export the postgres password so that subsequent commands don't ask for it
 export PGPASSWORD=$POSTGRES_PASSWORD
 
@@ -41,16 +33,16 @@ echo "-------------------------"
 # delete the db
 # deleting the db can fail. Spit out a comment if this happens but continue since the db
 # is created in the next step
-echo "deleting old database $POSTGRES_USER"
-if dropdb -h postgres -U $POSTGRES_USER $POSTGRES_USER
+echo "deleting old database django"
+if dropdb -h postgres -U $POSTGRES_USER django
 then echo "deleted $POSTGRES_USER database"
-else echo "database $POSTGRES_USER does not exist, continue"
+else echo "database django does not exist, continue"
 fi
 
 # create a new database
 echo "creating new database $POSTGRES_USER"
-createdb -h postgres -U $POSTGRES_USER $POSTGRES_USER -O $POSTGRES_USER
+createdb -h postgres -U $POSTGRES_USER django -O $POSTGRES_USER
 
 # restore the database
 echo "restoring database $POSTGRES_USER"
-gunzip -c $BACKUPFILE | psql -h postgres -U $POSTGRES_USER
+gunzip -c $BACKUPFILE | psql -h postgres -U $POSTGRES_USER django
