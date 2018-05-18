779b13c60e82af27b159df6923fecee72baabe9c
mervent <lamkaslim@gmail.com>
2018-05-18 22:11:30 +0300

    Update dev.yml so we can make and restore backups

--- {{cookiecutter.project_slug}}/docker/dev/django/start.sh
+++ {{cookiecutter.project_slug}}/docker/dev/django/start.sh
@@ -6,4 +6,7 @@ set -o xtrace
 
 
 python manage.py migrate
-python manage.py runserver_plus 0.0.0.0:8000
+until python manage.py runserver_plus 0.0.0.0:8000; do
+    echo "Restart django" >&2
+    sleep 3
+done
